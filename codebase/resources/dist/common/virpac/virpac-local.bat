ea60d394011a6d5e627d1b8b91c273dc15349576
Tim Pokorny <tim.pokorny@calytrix.com>
2016-10-08 21:35:22 +0800

    Removing virpac configs we no longer need.

--- codebase/resources/dist/common/virpac/virpac-local.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set DIR=%~dp0
-set DISCO_HOME=%DIR%..
-rem set PATH=%DISCO_HOME%\jre\bin;%PATH%
-set PATH=../jre/bin/;%PATH%
-java -jar "%DISCO_HOME%\lib\disco.jar" --app:distributor %* --config-file virpac-local.config
