7a496b5cc6016ed1028bd64d4ce3075a32309236
Tim Pokorny <tim.pokorny@calytrix.com>
2017-08-10 21:17:13 +0800

    Add fixes to launchers for finding the local JRE (Fix #20)

--- codebase/resources/dist/common/bin/disruptor.bat
+++ codebase/resources/dist/common/bin/disruptor.bat
@@ -1,5 +1,5 @@
 @echo off
 set DIR=%~dp0
 set DISCO_HOME=%DIR%..
-rem set PATH=%DISCO_HOME%\jre\bin;%PATH%
+set PATH=%DISCO_HOME%\jre\bin;%PATH%
 java -jar "%DISCO_HOME%\lib\disco.jar" --app:disruptor %*
