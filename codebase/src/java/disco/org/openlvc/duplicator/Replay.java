d18fefd6c2cf564b49b93f78782de92dde4dea6a
Tim Pokorny <tim.pokorny@calytrix.com>
2017-08-10 22:00:46 +0800

    Fix bug causing "infinite" duplicator replays to end after one iteration

--- codebase/src/java/disco/org/openlvc/duplicator/Replay.java
+++ codebase/src/java/disco/org/openlvc/duplicator/Replay.java
@@ -147,7 +147,7 @@ public class Replay implements IPduListener
 			logger.info( "Starting replay loop %d of %s", loopsCompleted+1, loopString );
 
 			this.replaySession();
-			if( ++loopsCompleted >= loopCount )
+			if( loopCount != 0 && (++loopsCompleted >= loopCount) )
 				loopAgain = false;
 		}
 		while( loopAgain );
