ea84368ace09a90318c5222ac9cd913cf3f6a332
Tim Pokorny <tim.pokorny@calytrix.com>
2020-06-03 11:06:01 +0800

    Add tracking of site/app ID to DIS configuration (not used).
    These values aren't used explicitly yet, but we can at least track them.
    At the moment, a user still has to set the values directly on the PDUs
    that are relevant.

--- codebase/src/java/disco/org/openlvc/disco/configuration/DisConfiguration.java
+++ codebase/src/java/disco/org/openlvc/disco/configuration/DisConfiguration.java
@@ -17,6 +17,8 @@
  */
 package org.openlvc.disco.configuration;
 
+import java.util.Random;
+
 /**
  * General DIS protocol settings that are applicable regardless of transport, sender or receiver
  * implementation.
@@ -27,7 +29,9 @@ public class DisConfiguration
 	//                    STATIC VARIABLES
 	//----------------------------------------------------------
 	// Keys for properties file
-	private static final String PROP_EXERCISE_ID  = "disco.dis.exerciseId";
+	private static final String PROP_EXERCISE_ID  = "disco.dis.exerciseId"; 
+	private static final String PROP_SITE_ID      = "disco.dis.siteId";
+	private static final String PROP_APP_ID       = "disco.dis.appId";
 
 	//----------------------------------------------------------
 	//                   INSTANCE VARIABLES
@@ -35,6 +39,8 @@ public class DisConfiguration
 	private DiscoConfiguration parent;
 
 	private short exerciseId;
+	private int   siteId;
+	private int   appId;
 
 	//----------------------------------------------------------
 	//                      CONSTRUCTORS
@@ -44,13 +50,15 @@ public class DisConfiguration
 		this.parent = parent;
 
 		this.exerciseId = -1; // -1 so we can lazy load
+		this.siteId     = Integer.MIN_VALUE; // will cause lazy load
+		this.appId      = Integer.MIN_VALUE; // will cause lazy load
 	}
 
 	//----------------------------------------------------------
 	//                    INSTANCE METHODS
 	//----------------------------------------------------------
 	/**
-	 * Exercise ID to use for outgoing packets. Defaults to 1.
+	 * @return Exercise ID to use for outgoing packets. Defaults to 1.
 	 * 
 	 * A setting of 0 indicates that we should accept traffic from anywhere.
 	 */
@@ -71,6 +79,75 @@ public class DisConfiguration
 		parent.setProperty( PROP_EXERCISE_ID, ""+exerciseId );
 	}
 
+	/**
+	 * @return The SiteID that should be used for PDUs. Range is 16-bit unsigned int.
+	 */
+	public int getSiteId()
+	{
+		// lazy load
+		if( this.siteId == Integer.MIN_VALUE )
+			this.siteId = Integer.parseInt( parent.getProperty(PROP_SITE_ID,"-1") );
+		
+		// generate if required
+		if( siteId == -1 )
+			siteId = new Random().nextInt( 65534 ); // keep away from ALL_SITES spec constant
+		
+		return siteId;
+	}
+
+	/**
+	 * Set the Site ID for this application. Valid values are:
+	 * <ul>
+	 *   <li>0: No Site</li>
+	 *   <li>1-65534: Regular range</li>
+	 *   <li>-1: Randomly generate</li>
+	 * </ul>
+	 * 
+	 * @param siteId The Site ID
+	 */
+	public void setSiteId( int siteId )
+	{
+		if( siteId < -1 || siteId > 65534 )
+			throw new IllegalArgumentException( "Site ID valid range is 0-65534 (-1 to randomly generate)" );
+		
+		this.siteId = siteId;
+	}
+	
+	/**
+	 * @return The ApplicationID that should be used for PDUs. Range is 16-bit unsigned int.
+	 */
+	public int getAppId()
+	{
+		// lazy load
+		if( this.appId == Integer.MIN_VALUE )
+			this.appId = Integer.parseInt( parent.getProperty(PROP_APP_ID,"-1") );
+		
+		// generate if required
+		if( this.appId == -1 )
+			this.appId = new Random().nextInt( 65534 ); // keep away from ALL_APPLIC spec contant
+		
+		return appId;
+	}
+	
+	/**
+	 * Set the Application ID for this application. Valid values are:
+	 * <ul>
+	 *   <li>0: No Application</li>
+	 *   <li>1-65534: Regular range</li>
+	 *   <li>-1: Randomly generate</li>
+	 * </ul>
+	 * 
+	 * @param siteId The Site ID
+	 */
+	public void setAppId( int appId )
+	{
+		if( appId < -1 || appId > 65534 )
+			throw new IllegalArgumentException( "App ID valid range is 0-65534 (-1 to randomly generate)" );
+		
+		this.appId = appId;
+	}
+	
+	
 	//----------------------------------------------------------
 	//                     STATIC METHODS
 	//----------------------------------------------------------
