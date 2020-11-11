openlvc/disco

commit 5e3d5d021c6c7cf36a44c28014b59aa2233b3f05

Fix bug causing Disrespector DIS traffic to loop back.

codebase/src/java/disco/org/openlvc/disco/utils/NetworkUtils.java
@@ -405,12 +405,14 @@ public class NetworkUtils
 	 */
 	public static DatagramSocket[] createBroadcastPair( InetAddress address,
 	                                                    int port,
+	                                                    NetworkInterface nic,
 	                                                    SocketOptions options )
 	{
 		try
 		{
 			// Create the send socket
-			DatagramSocket sendSocket = new DatagramSocket(null);
+			// Bind to ephemeral port (packets will have a trgt port) and ip-address of given nic
+			DatagramSocket sendSocket = new DatagramSocket( 0, getFirstIPv4Address(nic) );
 			sendSocket.setReuseAddress( true );
 			sendSocket.setBroadcast( true );
 			if( options != null )
@@ -420,15 +422,14 @@ public class NetworkUtils
 			}
 			
 			// Create the receive socket
-			DatagramSocket recvSocket = new DatagramSocket(null);
+			// Bind to receive DIS port and IP address of given nic
+			DatagramSocket recvSocket = new DatagramSocket( 0, getFirstIPv4Address(nic) );
 			recvSocket.setReuseAddress( true );
 			recvSocket.setBroadcast( true );
 			if( options != null )
 				recvSocket.setReceiveBufferSize( options.getRecvBufferSize() );
 			
 			// bind the two sockets
-			sendSocket.bind( new InetSocketAddress(0) ); // ephermal port
-			recvSocket.bind( new InetSocketAddress(address,port) );
 			return new DatagramSocket[] { sendSocket, recvSocket };
 		}
 		catch( Exception e )

codebase/src/java/disco/org/openlvc/disco/connection/UdpConnection.java
@@ -162,7 +162,7 @@ public class UdpConnection implements IConnection
 			logger.info( "Connecting broadcast socket - %s:%d (interface: %s)", address, port, networkInterface );
 			
 			// Create the socket pair
-			DatagramSocket[] pair = NetworkUtils.createBroadcastPair( address, port, options );
+			DatagramSocket[] pair = NetworkUtils.createBroadcastPair( address, port, networkInterface, options );
 			this.sendSocket = pair[0];
 			this.recvSocket = pair[1];
 		}

codebase/src/java/disco/org/openlvc/disco/connection/rpr/mappers/EntityStateMapper.java
@@ -361,7 +361,6 @@ public class EntityStateMapper extends AbstractMapper
 	@EventHandler
 	public void handlePdu( EntityStatePdu pdu )
 	{
-System.out.println( "ENTITY STATE: "+pdu.getMarking() );
 		// Do we already have an object cached for this entity?
 		PhysicalEntity hlaObject = objectStore.getLocalEntity( pdu.getEntityID() );
 
@@ -789,12 +788,6 @@ System.out.println( "ENTITY STATE: "+pdu.getMarking() );
 			
 			// Request an attribute update for the object so that we can get everything we need
 			super.requestAttributeUpdate( hlaObject );
-			if( logger.isTraceEnabled() )
-			{
-				logger.trace( "hla >> dis (Discover) Requested attribute update for [%d] attributes of [%s]",
-				              hlaObject.getObjectAttributes().size(),
-				              hlaObject.getObjectHandle().toString() );
-			}
 		}
 	}
 

commit 63a73b792c1b7b5a0dd0d3d4a1e1729a02931f77

Change broadcast receive socket to bind to specified DIS port
- The socket used for receiving packets was being bound to an
  ephemeral port (looks to have been a copy/paste from the sending
  socket which can have an ephermeral port set). Bug looks to have been
  introduced in commit 5e3d5d0 (4 Aug 2020) when the construction of
  sockets was changed, and explicit binding to a port was removed.
- Added back in binding to the specified DIS port on the receive socket.

codebase/src/java/disco/org/openlvc/disco/utils/NetworkUtils.java
@@ -423,7 +423,7 @@ public class NetworkUtils
 			
 			// Create the receive socket
 			// Bind to receive DIS port and IP address of given nic
-			DatagramSocket recvSocket = new DatagramSocket( 0, getFirstIPv4Address(nic) );
+			DatagramSocket recvSocket = new DatagramSocket( port, getFirstIPv4Address(nic) );
 			recvSocket.setReuseAddress( true );
 			recvSocket.setBroadcast( true );
 			if( options != null )